675d9dd97a3eb47ec1cb9d395ace213a0bd8e0ac
Tash1gi <gleb.semenov06@mail.ru>
2024-01-22 23:29:40 +0500

    Добавил файл 1 Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.import java.util.Scanner;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число:");
+        float num1 = scan.nextFloat();
+        System.out.println("Ваше число:" + num1);
+
+    }
+
+}
