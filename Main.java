b841d0fa9f8d94c141f5c242dfb1f7cb3075fc27
Tash1gi <gleb.semenov06@mail.ru>
2024-01-23 23:00:27 +0500

    add calculator

--- Main.java
+++ Main.java
@@ -1,14 +1,56 @@
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.import java.util.Scanner;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String [] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите число:");
-        float num1 = scan.nextFloat();
-        System.out.println("Ваше число:" + num1);
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        //Пользователь должен ввести свое первое число
+
+        System.out.println("Введите первое число");
+        float num1 = scanner.nextFloat();
+g
+        //Пользователь должен ввести свое второе число
+
+        System.out.println("Введите второе число");
+        float num2 = scanner.nextFloat();
+
+        //Пользователь должен ввести операцию (+, -,*, /)
+
+        System.out.println("Введите операцию");
+
+        char operation = scanner.next().charAt(0);
+
+        scanner.close();
+        double out = 0.0;
+
+        switch (operation) {
+            case '+':
+                out = num1 + num2;
+                break;
+            case '-':
+                out = num1 - num2;
+                break;
+            case '*':
+                out = num1 * num2;
+                break;
+        }
+        try {
+            out = num1 / num2;
+        } catch (ArithmeticException e) {
+            System.out.println("на ноль делить нельзя.");
+        }
+        break;
+        default:
+        System.out.println("Неправильно введите ещё раз.");
+        g
+    }
+    }
+
+
+}
+
+        System.out.println(num1 + " " + operation + " " + num2 + " = " + out);
     }
 
 }
